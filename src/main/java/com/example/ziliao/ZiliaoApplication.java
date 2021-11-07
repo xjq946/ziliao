cc7939e1891229285284fd52712da9634d6acaa0
xiejiqing <xjq111006@163.com>
2021-11-07 11:50:27 +0800

    Initial commit

---
+++ src/main/java/com/example/ziliao/ZiliaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ziliao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiliaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiliaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ziliao/ZiliaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ziliao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiliaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
